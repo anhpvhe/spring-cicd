115f86c657de694310f70f2e21e241993d0a2490
Githnewbie <anhpvhe176182@fpt.edu.vn>
2024-05-10 06:55:39 +0700

    first commit

---
+++ src/main/java/com/anhpv/springcicd/ProductController.java
@@ -0,0 +1,25 @@
+package com.anhpv.springcicd;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(path = "/list")
+    public @ResponseBody Iterable<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewProduct(@RequestParam String name){
+        Product p = new Product();
+        p.setName(name);
+        productRepository.save(p);
+        return "Saved";
+    }
+}
---
+++ src/main/java/com/anhpv/springcicd/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.anhpv.springcicd;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
