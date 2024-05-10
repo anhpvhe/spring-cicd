ebbbcf51caa919bcde443b99feda422b33c51bcb
Githnewbie <anhpvhe176182@fpt.edu.vn>
2024-05-10 08:43:39 +0700

    Update app.properties

--- src/main/java/com/anhpv/springcicd/SpringCicdApplication.java
+++ src/main/java/com/anhpv/springcicd/SpringCicdApplication.java
@@ -2,9 +2,15 @@ package com.anhpv.springcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringCicdApplication {
+
+    @GetMapping("/hello")
+    public String welcome(){
+        return "Hello world!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCicdApplication.class, args);
